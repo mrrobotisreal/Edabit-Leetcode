c8f09aeb57c45d4a791bb6d2360e51078c6fa59a
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-05 14:38:38 -0600

    Added VIPCustomer class to BankAccount in Java.

---
+++ java/LearnClasses/BankAccount/VIPCustomer.java
@@ -0,0 +1,21 @@
+package java.LearnClasses.BankAccount;
+
+public class VIPCustomer {
+  private String name;
+  private double limit;
+  private String email;
+
+  public VIPCustomer() {
+    this("Default Name", 1000, "Default Email");
+  }
+
+  public VIPCustomer(String name, String email) {
+    this(name, 1000, email);
+  }
+
+  public VIPCustomer(String name, double limit, String email) {
+    this.name = name;
+    this.limit = limit;
+    this.email = email;
+  }
+}
