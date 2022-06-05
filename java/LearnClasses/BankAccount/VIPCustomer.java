bb95ca69227235963d90e82e6024dabe7a511f25
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-05 14:43:04 -0600

    Updated VIPCustomer class to include getters in BankAccount in Java.

--- java/LearnClasses/BankAccount/VIPCustomer.java
+++ java/LearnClasses/BankAccount/VIPCustomer.java
@@ -6,16 +6,28 @@ public class VIPCustomer {
   private String email;
 
   public VIPCustomer() {
-    this("Default Name", 1000, "Default Email");
+    this("Default Name", 1000.00, "Default Email");
   }
 
   public VIPCustomer(String name, String email) {
-    this(name, 1000, email);
+    this(name, 1000.00, email);
   }
 
   public VIPCustomer(String name, double limit, String email) {
     this.name = name;
     this.limit = limit;
     this.email = email;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public double getLimit() {
+    return this.limit;
+  }
+
+  public String getEmail() {
+    return this.email;
   }
 }
